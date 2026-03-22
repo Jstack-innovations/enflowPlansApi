c4f6d5cb64d19240c62d459da6c27e3d80e84eb6
JStack Innovations <wsamson630@gmail.com>
2026-03-22 15:13:18 +0100

    Update verify.php

--- api/POST/AUTH/verify.php
+++ api/POST/AUTH/verify.php
@@ -1,11 +1,13 @@
 <?php
+header("Content-Type: text/html; charset=UTF-8");
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
    http_response_code(200);
-    exit();
+   exit();
 }
 
 $file = __DIR__ . '/../../SECURE/db.php';
