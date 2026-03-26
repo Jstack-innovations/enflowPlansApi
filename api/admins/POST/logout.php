a125a3c17a1920361af06cfbe93faec1802980ec
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:37:09 +0100

    Update logout.php

--- api/admins/POST/logout.php
+++ api/admins/POST/logout.php
@@ -1,8 +1,18 @@
 <?php
 require_once __DIR__ . "/../../SECURE/authGuard.php";
+
 // destroy session
 session_unset();
 session_destroy();
+
+// remove session cookie from browser
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 
 echo json_encode([
   "success" => true,
