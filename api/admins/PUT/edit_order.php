87c1d2ed522c3567616d67f379b5f9f72b13b048
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:40:04 +0100

    Update edit_order.php

--- api/admins/PUT/edit_order.php
+++ api/admins/PUT/edit_order.php
@@ -1,21 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, PUT, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
-session_start();
-
-/*if (!isset($_SESSION['admin_id'])) {
-    http_response_code(401);
-    echo json_encode(["error" => "Unauthorized"]);
-    exit;
-}*/
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 
 $file = __DIR__ . '/../../SECURE/db.php';
 
