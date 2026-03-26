f1474d873deacefbcb7d7d5af56c104c8eace259
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:08:49 +0100

    Update fetch_order.php

--- api/admins/GET/fetch_order.php
+++ api/admins/GET/fetch_order.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-header("Content-Type: application/json");
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 
 require_once __DIR__ . '/../../SECURE/config.php';
 
