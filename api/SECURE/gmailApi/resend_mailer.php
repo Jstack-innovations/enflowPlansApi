7358f418818f3fb3d741ff1c1e4b1ab6e31efdc6
JStack Innovations <wsamson630@gmail.com>
2026-04-27 06:25:24 +0100

    Create resend_mailer.php

---
+++ api/SECURE/gmailApi/resend_mailer.php
@@ -0,0 +1,37 @@
+<?php
+
+function sendEmail($to, $subject, $body) {
+
+    $apiKey = getenv("RESEND_API_KEY");
+
+    $data = [
+        "from" => "Grillux <onboarding@resend.dev>", 
+        "to" => is_array($to) ? $to : [$to],
+        "subject" => $subject,
+        "html" => $body
+    ];
+
+    $ch = curl_init("https://api.resend.com/emails");
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer " . $apiKey,
+        "Content-Type: application/json"
+    ]);
+
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        curl_close($ch);
+        return ["status" => "error", "message" => $error];
+    }
+
+    curl_close($ch);
+
+    return json_decode($response, true);
+}
+
