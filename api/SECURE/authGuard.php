f207cc696ed73df496e82968297f265cb8722524
jStack●Innovations <wsamson630@gmail.com>
2026-05-23 21:35:33 +0100

    Add new allowed origin for CORS

--- api/SECURE/authGuard.php
+++ api/SECURE/authGuard.php
@@ -15,7 +15,7 @@ session_start();
 $allowedOrigins = [
     "http://localhost:5173",
     "https://artisangrills-production.up.railway.app",
-    "https://artisangrills-production.up.railway.app",
+    "https://artisangrills.onrender.com",
     "https://admin-artisangrilluxe.vercel.app"
 ];
 
